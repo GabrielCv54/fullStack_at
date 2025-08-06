f8d2904864e4e5a93fef602dc4acef06647c5774
Gabriel <gabriel.carvalhovieira123@gmail.com>
2025-08-06 14:48:26 -0300

    commit final

--- calculadora.imc/script.js
+++ calculadora.imc/script.js
@@ -1,27 +1,28 @@
 function IMC(){
-    var height = document.querySelector('input#height');
-    var hValue = height.value
-    var weight = document.querySelector('input#weight');
-    var wValue = weight.value
+    var height = Number.parseFloat(document.querySelector('input#height').value);
+    var weight = Number.parseFloat(document.querySelector('input#weight').value);
     var result = document.querySelector('div#result');
-    var calc = (hValue / (wValue** 2))
+    let calc = (weight / (height ** 2))
 
-   if(calc >= 17 && calc < 18.49){
+   if(calc < 17){
+    result.style.color ='red'
+    result.textContent = `Muito abaixo do Peso` + `Seu IMC: ${calc}`
+   } else if(calc >= 17 && calc < 18.49){
     result.style.color  = 'yellow'
-    result.textContent = 'Abaixo do Peso' + `Seu IMC: ${calc}`
+    result.textContent = 'Abaixo do Peso ' + `Seu IMC: ${calc.toFixed(2)}`
    } else if(calc >= 18.5 && calc < 24.99){
-    result.style.color = 'green'
-    result.textContent = 'Peso normal : ' + `Seu IMC: ${calc}`
+    result.style.color = 'greenyellow'
+    result.textContent = 'Peso normal  ' + `Seu IMC: ${calc.toFixed(2)}`
    }else if(calc >= 25 && calc < 29.99){
      result.style.color = 'orange'
-     result.textContent = 'Sobrepeso' + `Seu IMC: ${calc}`
+     result.textContent = 'Sobrepeso ' + `Seu IMC: ${calc.toFixed(2)}`
    }else if (calc > 30){
     result.style.color = 'red'
-    result.textContent = 'Obesidade'+ `Seu IMC: ${calc}`
-   }else if (hValue == NaN || wValue == NaN){
+    result.textContent = 'Obesidade '+ `Seu IMC: ${calc.toFixed(2)}`
+   }else if (isNaN(height) || isNaN(weight)){
     alert('É preciso que valores sejam digitados para calcular!')
    }
-}
 
-var button = document.getElementById('btn');
-button.addEventListener('click',IMC)
+
+
+  }
